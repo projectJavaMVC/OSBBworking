dabdc59887592ebcd2b5b66cb2bfb203b098c851
maxim <projectjavamvc@gmail.com>
2015-11-24 15:48:41 +0200

    FirstCommit

---
+++ src/main/java/ua/kiev/prog/DAO/BuildDAO.java
@@ -0,0 +1,12 @@
+package ua.kiev.prog.DAO;
+
+import ua.kiev.prog.entity.BuildsEntity;
+
+/**
+ * Created by m.bratyuk on 23.11.2015.
+ */
+public interface BuildDAO {
+    void addBuild (BuildsEntity build);
+    BuildsEntity getIDByKey (String key);
+    void mergeBuild (BuildsEntity build);
+}
---
+++ src/main/java/ua/kiev/prog/entity/UserEntity.java
@@ -0,0 +1,82 @@
+package ua.kiev.prog.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by m.bratyuk on 23.11.2015.
+ */
+@Entity
+@Table(name = "users", catalog = "prog")
+public class UserEntity {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String login;
+    private String pass;
+    private String email;
+    private Short type;
+
+
+  @ManyToOne(targetEntity = BuildsEntity.class)
+  @JoinColumn(name = "build_id")
+    private BuildsEntity buildsEntity;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String login, String pass, String email, Short type, BuildsEntity buildsEntity) {
+        this.login = login;
+        this.pass = pass;
+        this.email = email;
+        this.type = type;
+        this.buildsEntity = buildsEntity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Short getType() {
+        return type;
+    }
+
+    public void setType(Short type) {
+        this.type = type;
+    }
+
+    public BuildsEntity getBuildsEntity() {
+        return buildsEntity;
+    }
+
+    public void setBuildsEntity(BuildsEntity buildsEntity) {
+        this.buildsEntity = buildsEntity;
+    }
+}
