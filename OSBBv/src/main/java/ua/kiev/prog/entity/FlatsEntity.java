96a8af77d39998cce6c4bdffcd07bf28ea064340
maxim <projectjavamvc@gmail.com>
2015-11-25 17:10:24 +0200

    FlatsEntity

--- OSBBv/src/main/java/ua/kiev/prog/entity/FlatsEntity.java
+++ OSBBv/src/main/java/ua/kiev/prog/entity/FlatsEntity.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
  */
 @Entity
 @Table(name = "flats", schema = "", catalog = "prog")
-public class FlatsEntity {
+    public class FlatsEntity {
     @Id
     @GeneratedValue
     private long id;
