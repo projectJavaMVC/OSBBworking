b526fdeea7a26f3eecf8a470a21b71fe08d198a2
projectjavamvc <projectjavamvc@gmail.com>
2015-12-04 18:06:18 +0200

    Full commit #11

---
+++ OSBB_SpringData_v1.1/src/main/java/ua/kiev/prog/entity/ServicesEntity.java
@@ -0,0 +1,59 @@
+package ua.kiev.prog.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mbro8_000 on 26.11.2015.
+ */
+@Entity
+@Table(name = "services")
+public class ServicesEntity {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+
+    @OneToMany(mappedBy = "servicesEntity", targetEntity = BuildServices.class)
+    private List<BuildServices> buildservices = new ArrayList<BuildServices>();
+
+    public ServicesEntity(String name) {
+        this.name = name;
+    }
+
+    public ServicesEntity() {
+
+    }
+
+
+    public List<BuildServices> getBuildservices() {
+        return buildservices;
+    }
+
+    public void setBuildservices(List<BuildServices> buildservices) {
+        this.buildservices = buildservices;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.id).append(" ").append(this.name);
+        return sb.toString();
+    }
+}
